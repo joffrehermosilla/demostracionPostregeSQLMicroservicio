817539ffd21625541cb58d2acfbb5b44d51eec3c
joffre <joffre.hermosilla@gmail.com>
2022-01-08 15:26:16 -0500

    Segundo Commit inclusión de Spring Gateway prueba exitosa del crud

---
+++ demostracionPostregeSQLMicroservicio/microservicios-gateway/src/test/java/com/joffre/microservicios/app/gateway/MicroserviciosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joffre.microservicios.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
